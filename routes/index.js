ebe20bf7f6d993dcc4c1a14b61ef3c9a2e95aaba
jungjihyoun <jihyoun0602@gmail.com>
2021-11-17 22:45:09 +0900

    test code

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,56 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/test", function (req, res, next) {
-  maria.query("select * from t_product", function (err, rows, fields) {
+router.get("/users", function (req, res, next) {
+  maria.query("select * from user", function (err, rows, fields) {
     if (!err) {
+      console.log(rows);
+      console.log(fields);
       var result =
-        "rows : " + JSON.stringify(rows) + "fields : " + JSON.stringify(fields);
-      res.send(rows);
+        "rows : " +
+        JSON.stringify(rows) +
+        "<br><br>" +
+        "fields : " +
+        JSON.stringify(fields);
+      res.send(result);
     } else {
       console.log("query error : " + err);
-      res.json(err);
+      res.send(err);
     }
   });
 });
+
+router.get("/post", function (req, res, next) {
+  maria.query("select * from post", function (err, rows, fields) {
+    if (!err) {
+      console.log(rows);
+      console.log(fields);
+      var result =
+        "rows : " +
+        JSON.stringify(rows) +
+        "<br><br>" +
+        "fields : " +
+        JSON.stringify(fields);
+      res.send(result);
+    } else {
+      console.log("query error : " + err);
+      res.send(err);
+    }
+  });
+});
+
+router.post("/write/user", function (req, res, next) {
+  maria.query(
+    "INSERT INTO user (user_id,password,username,birthday,email) VALUES('tester02','tester~','tester','1998-06-02','tester@gmail.com')",
+    function (err, rows, fields) {
+      if (!err) {
+        console.log(result);
+      } else {
+        console.log("query error : " + err);
+        res.send(err);
+      }
+    }
+  );
+});
+
 module.exports = router;
