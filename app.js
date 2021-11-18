3da3296ee97f55194ed44d35b615049acb839b63
jungjihyoun <jihyoun0602@gmail.com>
2021-11-18 14:56:06 +0900

    feat/ check email api and validate code api

--- app.js
+++ app.js
@@ -4,21 +4,23 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+// router
+const auth = require("./routes/auth");
+app.use("/auth", auth);
+
+const topic = require("./routes/dashBoard");
+app.use("/dashBoard", topic);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/dashBoard.js
+++ routes/dashBoard.js
@@ -1,41 +1,9 @@
 var express = require("express");
 var router = express.Router();
-
-// requiree maria.js
 const maria = require("../database/connect/maria");
 
 // ******************
-const nodemailer = require("nodemailer");
 
-const smtpTransport = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: "jihyoun0602@gmail.com",
-    pass: "wlgus8402@",
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
-
-router.post("/emailAuth", async (req, res) => {
-  const mailOptions = {
-    from: "jihyoun0602@gmail.com",
-    to: "jihyoun0602@naver.com",
-    subject: "인증받기",
-    text: "메일 내용 테스트 ! ",
-  };
-
-  await smtpTransport.sendMail(mailOptions, (error, responses) => {
-    if (error) {
-      res.json({ test: error });
-    } else {
-      res.json({ msg: "sucess" });
-    }
-    smtpTransport.close();
-  });
-});
-// ********
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
@@ -77,20 +45,6 @@ router.get("/post", function (req, res, next) {
       res.send(err);
     }
   });
-});
-
-router.post("/write/user", function (req, res, next) {
-  maria.query(
-    "INSERT INTO user (user_id,password,username,birthday,email) VALUES('tester02','tester~','tester','1998-06-02','tester@gmail.com')",
-    function (err, rows, fields) {
-      if (!err) {
-        console.log(result);
-      } else {
-        console.log("query error : " + err);
-        res.send(err);
-      }
-    }
-  );
 });
 
 module.exports = router;
